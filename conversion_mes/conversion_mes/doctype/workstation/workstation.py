1d8568534e36a8ce1c12822abf46b2911fa21ccf
Navneet Jain <navneetjain89@gmail.com>
2024-12-14 17:56:38 +0530

    DocType Development V1

---
+++ conversion_mes/conversion_mes/doctype/workstation/workstation.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, IndusWorks and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Workstation(Document):
+	pass
