2c76be82ebabcd17a67487711fd825cd1d45563a
Navneet Jain <navneetjain89@gmail.com>
2024-12-17 17:50:40 +0530

    Functionality Development V1

--- conversion_mes/conversion_mes/doctype/workstation/workstation.py
+++ conversion_mes/conversion_mes/doctype/workstation/workstation.py
@@ -1,9 +1,10 @@
 # Copyright (c) 2024, IndusWorks and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Workstation(Document):
-	pass
+	def before_save(self):
+		self.total_operating_cost = self.land_cost + self.labor_cost + self.energy_cost + self.other_cost
