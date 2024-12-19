1ffe68fb21439628a69721e5e778b7a4b432cdb0
Navneet Jain <navneetjain89@gmail.com>
2024-12-19 15:43:48 +0530

    Functionality Development V5

---
+++ conversion_mes/functions.py
@@ -0,0 +1,151 @@
+import frappe
+from frappe.utils import nowdate
+from datetime import datetime, timedelta
+
+
+
+@frappe.whitelist(allow_guest=True)
+def downtime_automation():
+    today_date = nowdate()
+    today_day = datetime.now().strftime("%A")
+    sites = frappe.get_all("Site", filters={"is_active": "1", "enable_downtime_automation": "1"})
+    for site in sites:
+        site = frappe.get_doc("Site", site.name)
+        is_today_working = is_working_day_checker(today_day, site)
+        if is_today_working == 1:
+            is_today_holiday, holiday_description = is_holiday_checker(today_date, site)
+            if is_today_holiday is True:
+                create_holiday_dt(today_date, site, holiday_description)
+            else:
+                create_no_shifts_dt(today_date, site)
+                create_breaks_dt(today_date, site)
+        if is_today_working == 0:
+            create_non_working_day_dt(today_date, site)
+            #create_no_shifts_dt(today_date, site)
+
+
+def is_working_day_checker(today_day, site):
+    if today_day == "Monday":
+        return site.monday
+    elif today_day == "Tuesday":
+        return site.tuesday
+    elif today_day == "Wednesday":
+        return site.wednesday
+    elif today_day == "Thursday":
+        return site.thursday
+    elif today_day == "Friday":
+        return site.friday
+    elif today_day == "Saturday":
+        return site.saturday
+    elif today_day == "Sunday":
+        return site.sunday
+    else:
+        return False
+    
+
+def is_holiday_checker(today_date, site):
+    holiday_list = frappe.get_doc("Holiday List", site.holiday_list)
+    print(f'Holiday List: {holiday_list.name}')
+    holidays = holiday_list.holidays
+    for holiday in holidays:
+        if str(holiday.holiday_date) == str(today_date):
+            holiday_description = holiday.description
+            return True, holiday_description
+    return False, None
+
+def create_holiday_dt(today_date, site, holiday_description):
+    day_start_time = site.start_time
+    day_end_time = site.end_time
+    reason = site.downtime_reason_for_holidays
+    start_datetime = datetime.strptime(f"{today_date} {day_start_time}", "%Y-%m-%d %H:%M:%S")
+    end_datetime = datetime.strptime(f"{today_date} {day_end_time}", "%Y-%m-%d %H:%M:%S")
+    if end_datetime <= start_datetime:
+        end_datetime += timedelta(days=1)
+    workstations = frappe.get_all("Workstation", filters={"site": site.name})
+    for workstation in workstations:
+        workstation = frappe.get_doc("Workstation", workstation.name)
+        downtime_log = frappe.new_doc("Downtime Log")
+        downtime_log.status = "Closed"
+        downtime_log.created_date = today_date
+        downtime_log.workstation = workstation.name
+        downtime_log.site = site.name
+        downtime_log.start_date_time = start_datetime
+        downtime_log.end_date_time = end_datetime
+        downtime_log.reason = reason
+        downtime_log.remarks = holiday_description
+        downtime_log.save()
+        frappe.db.commit()
+
+def create_no_shifts_dt(today_date, site):
+    next_day = datetime.strptime(today_date, "%Y-%m-%d") + timedelta(days=1)
+    day_start_time = site.start_time
+    day_end_time = site.end_time
+    reason = site.downtime_reason_for_non_working_hours
+    
+    # Ensure time strings are correctly formatted
+    start_datetime = datetime.strptime(f"{today_date} {day_end_time}", "%Y-%m-%d %H:%M:%S")
+    end_datetime = datetime.strptime(f"{next_day.strftime('%Y-%m-%d')} {day_start_time}", "%Y-%m-%d %H:%M:%S")
+    
+    workstations = frappe.get_all("Workstation", filters={"site": site.name})
+    for workstation in workstations:
+        workstation = frappe.get_doc("Workstation", workstation.name)
+        downtime_log = frappe.new_doc("Downtime Log")
+        downtime_log.status = "Closed"
+        downtime_log.created_date = today_date
+        downtime_log.workstation = workstation.name
+        downtime_log.site = site.name
+        downtime_log.start_date_time = start_datetime
+        downtime_log.end_date_time = end_datetime
+        downtime_log.reason = reason
+        downtime_log.remarks = "System Generated Downtime: No Shift Scheduled"
+        downtime_log.save()
+        frappe.db.commit()
+
+
+def create_non_working_day_dt(today_date, site):
+    day_start_time = site.start_time
+    day_end_time = site.end_time
+    reason = site.downtime_reason_for_non_working_days
+    start_datetime = datetime.strptime(f"{today_date} {day_start_time}", "%Y-%m-%d %H:%M:%S")
+    end_datetime = datetime.strptime(f"{today_date} {day_end_time}", "%Y-%m-%d %H:%M:%S")
+    if end_datetime <= start_datetime:
+        end_datetime += timedelta(days=1)
+    workstations = frappe.get_all("Workstation", filters={"site": site.name})
+    for workstation in workstations:
+        workstation = frappe.get_doc("Workstation", workstation.name)
+        downtime_log = frappe.new_doc("Downtime Log")
+        downtime_log.status = "Closed"
+        downtime_log.created_date = today_date
+        downtime_log.workstation = workstation.name
+        downtime_log.site = site.name
+        downtime_log.start_date_time = start_datetime
+        downtime_log.end_date_time = end_datetime
+        downtime_log.reason = reason
+        downtime_log.remarks = "System Generated Downtime: Non Working Day"
+        downtime_log.save()
+        frappe.db.commit()
+
+
+def create_breaks_dt(today_date, site):
+    planned_breaks = site.breaks
+    for planned_break in planned_breaks:
+        description = planned_break.description
+        reason = planned_break.downtime_reason
+        start_time = planned_break.start_time
+        end_time = planned_break.end_time
+        start_datetime = datetime.strptime(f"{today_date} {start_time}", "%Y-%m-%d %H:%M:%S")
+        end_datetime = datetime.strptime(f"{today_date} {end_time}", "%Y-%m-%d %H:%M:%S")
+        workstations = frappe.get_all("Workstation", filters={"site": site.name})
+        for workstation in workstations:
+            workstation = frappe.get_doc("Workstation", workstation.name)
+            downtime_log = frappe.new_doc("Downtime Log")
+            downtime_log.status = "Closed"
+            downtime_log.created_date = today_date
+            downtime_log.workstation = workstation.name
+            downtime_log.site = site.name
+            downtime_log.start_date_time = start_datetime
+            downtime_log.end_date_time = end_datetime
+            downtime_log.reason = reason
+            downtime_log.remarks = description
+            downtime_log.save()
+            frappe.db.commit()
