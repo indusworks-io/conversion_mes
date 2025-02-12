7db008de9d92be1f34b1a3fe4046b07b0a02e836
Navneet Jain <navneetjain89@gmail.com>
2025-02-12 16:50:58 +0530

    Updated Shift Log Duration Calculation Function

--- conversion_mes/conversion_mes/doctype/shift_log/shift_log.py
+++ conversion_mes/conversion_mes/doctype/shift_log/shift_log.py
@@ -7,21 +7,18 @@ from frappe.utils import now, time_diff_in_seconds, add_days
 from datetime import datetime
 
 class ShiftLog(Document):
-    def on_change(self):
+    def before_save(self):
         self.duration = 0
         if self.start_time and self.end_time:
-            if self.start_time > self.end_time:
-                print(f"Start Time: {self.start_time}")
-                today = self.date
-                tomorrow = add_days(today, 1)
-                start_datetime_str = f"{today} {self.start_time}"
-                end_datetime_str = f"{tomorrow} {self.end_time}"
-                start_datetime = datetime.strptime(start_datetime_str, '%Y-%m-%d %H:%M:%S')
-                end_datetime = datetime.strptime(end_datetime_str, '%Y-%m-%d %H:%M:%S')
+            print(f"Start Time: {self.start_time}")
+            print(f"End Time: {self.end_time}")
+            start_datetime_str = f"{self.date} {self.start_time}"
+            start_datetime = datetime.strptime(start_datetime_str, '%Y-%m-%d %H:%M:%S')
+            end_datetime_str = f"{self.date} {self.end_time}"
+            end_datetime = datetime.strptime(end_datetime_str, '%Y-%m-%d %H:%M:%S')
+            
+            if end_datetime < start_datetime:
+                end_datetime = add_days(end_datetime, 1)
                 self.duration = time_diff_in_seconds(end_datetime, start_datetime)
             else:
-                start_datetime_str = f"{self.date} {self.start_time}"
-                end_datetime_str = f"{self.date} {self.end_time}"
-                start_datetime = datetime.strptime(start_datetime_str, '%Y-%m-%d %H:%M:%S')
-                end_datetime = datetime.strptime(end_datetime_str, '%Y-%m-%d %H:%M:%S')
                 self.duration = time_diff_in_seconds(end_datetime, start_datetime)
